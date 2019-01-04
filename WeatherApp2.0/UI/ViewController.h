499322f64ccf33b9c21070cf911259bf33aa3511
Nodir <sinigami83@mail.ru>
2019-01-04 18:33:38 +0500

    added class for compare two dates

--- WeatherApp2.0/UI/ViewController.h
+++ WeatherApp2.0/UI/ViewController.h
@@ -6,6 +6,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
 @end
 
--- WeatherApp2.0/UI/ViewController.m
+++ WeatherApp2.0/UI/ViewController.m
@@ -4,16 +4,39 @@
 //
 
 #import "ViewController.h"
+#import "CompareTwoDates.h"
 
-@interface ViewController ()
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UILabel *temperatureInCityLable;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLable;
+@property (weak, nonatomic) IBOutlet UILabel *cityNameLable;
+@property (nonatomic, strong) NSNumber *cityID;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
+    self.cityID = @524901;
 }
 
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+
+    return cell;
+}
 
 @end
